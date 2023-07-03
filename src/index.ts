c533b2c8c99e8a111b51d2fb49707b7b1d7c83d7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-07-03 17:22:28 +0700

    chore: export options interface

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
-import { Options } from './types';
-import fromText from './translate/text';
-import fromDocs from './translate/docs';
-import buildQuery from './utils/buildQuery';
-import fromTextArray from './translate/textArray';
-import fromDocsArray from './translate/docsArray';
 import { errArrayLenZero, errInvalidType } from './error';
+import fromDocs from './translate/docs';
+import fromDocsArray from './translate/docsArray';
+import fromText from './translate/text';
+import fromTextArray from './translate/textArray';
+import { Options } from './types';
+import buildQuery from './utils/buildQuery';
 
-function translateText(
-	text: string | string[],
-	opt: Options
-): Promise<string | string[]> {
+function translateText(text: string | string[], opt: Options): Promise<string | string[]> {
 	if (typeof text === 'string')
-		return fromText(
-			buildQuery({ text, to: opt.to, from: opt.from, op: 'translate' }),
-			{ headless: opt.headless, timeout: opt.timeout }
-		);
+		return fromText(buildQuery({ text, to: opt.to, from: opt.from, op: 'translate' }), {
+			headless: opt.headless,
+			timeout: opt.timeout
+		});
 	if (Array.isArray(text)) {
 		if (text.length === 0) return errArrayLenZero();
 		const queryArr: string[] = [];
@@ -33,19 +30,12 @@ function translateText(
 	return errInvalidType(typeof text);
 }
 
-function translateDocs(
-	path: string | string[],
-	opt: Options
-): Promise<string | string[]> {
+function translateDocs(path: string | string[], opt: Options): Promise<string | string[]> {
 	if (typeof path === 'string') {
-		return fromDocs(
-			buildQuery({ to: opt.to, from: opt.from, op: 'docs' }),
-			path,
-			{
-				headless: opt.headless,
-				timeout: opt.timeout
-			}
-		);
+		return fromDocs(buildQuery({ to: opt.to, from: opt.from, op: 'docs' }), path, {
+			headless: opt.headless,
+			timeout: opt.timeout
+		});
 	}
 	if (Array.isArray(path)) {
 		if (path.length === 0) return errArrayLenZero();
@@ -64,4 +54,4 @@ function translateDocs(
 	return errInvalidType(typeof path);
 }
 
-export { translateText, translateDocs };
+export { translateText, translateDocs, Options as PuppeteerTranslateOptions };

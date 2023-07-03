953215bb73c2a0364351a53301e6bccfc07dde1c
dimaslanjaka <dimaslanjaka@gmail.com>
2023-07-03 17:36:55 +0700

    refactor: disable eslint rule no-useless-catch

--- src/translate/text.ts
+++ src/translate/text.ts
@@ -1,33 +1,24 @@
 import launch from '../launch';
 import { PuppeteerOptions } from '../types';
 
-export default async (
-	query: string,
-	opt: PuppeteerOptions
-): Promise<string> => {
+export default async (query: string, opt: PuppeteerOptions): Promise<string> => {
 	const { browser, page, timeout } = await launch(opt);
 	try {
 		await page.goto('https://translate.google.com/' + query);
-		const el1 = await page.waitForSelector(
-			'span>span[data-phrase-index="0"]>span[jsaction]',
-			{
-				timeout
-			}
-		);
+		const el1 = await page.waitForSelector('span>span[data-phrase-index="0"]>span[jsaction]', {
+			timeout
+		});
 		const texts = [await el1.evaluate((e) => e.textContent)];
-		const len = (await page.$$('span>span[data-phrase-index]>span[jsaction]'))
-			.length;
+		const len = (await page.$$('span>span[data-phrase-index]>span[jsaction]')).length;
 		if (len === 1) return texts[0];
 		for (let i = 1; i < len; i++) {
-			const el = await page.waitForSelector(
-				`span>span[data-phrase-index="${i}"]>span[jsaction]`,
-				{
-					timeout
-				}
-			);
+			const el = await page.waitForSelector(`span>span[data-phrase-index="${i}"]>span[jsaction]`, {
+				timeout
+			});
 			texts.push(await el.evaluate((e) => e.textContent));
 		}
 		return texts.join(' ');
+		// eslint-disable-next-line no-useless-catch
 	} catch (err) {
 		throw err;
 	} finally {
